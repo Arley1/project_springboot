03d2b590c1754db516104874c2655eb3b6f196f5
Arley1 <564952562@qq.com>
2022-10-14 08:42:25 +0800

    增加订单service、mapper实现，增加aspect

--- src/main/java/dev/com/AppMain.java
+++ src/main/java/dev/com/AppMain.java
@@ -2,6 +2,7 @@ package dev.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
